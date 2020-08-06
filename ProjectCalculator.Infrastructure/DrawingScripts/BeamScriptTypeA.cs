7fc0e37f842290548f13a4a0457adfbbd8ee4675
orzio <andrzej.orzinsk@gmail.com>
2020-08-06 20:49:05 +0200

    add beam scriptor typeA

--- ProjectCalculator.Infrastructure/DrawingScripts/BeamScriptTypeA.cs
+++ ProjectCalculator.Infrastructure/DrawingScripts/BeamScriptTypeA.cs
@@ -6,12 +6,12 @@ using System.Text;
 
 namespace ProjectCalculator.Infrastructure.DrawingScripts
 {
-    public class BeamScriptTypeA: IBeamScript
+    public class BeamScriptTypeA : IBeamScript
     {
         private InternalForces _internalForces;
-        private Beam _beam; 
+        private Beam _beam;
 
-        public BeamScriptTypeA(InternalForces internalForces,Beam beam )
+        public BeamScriptTypeA(InternalForces internalForces, Beam beam)
         {
             _internalForces = internalForces;
             _beam = beam;
@@ -29,9 +29,13 @@ namespace ProjectCalculator.Infrastructure.DrawingScripts
 
 
                   DrawBeam() +
-                  DrawSupport()+
-                  DrawHorizontalDimensions()+
-                  DrawSupportForces()+
+                  DrawSupport() +
+                  DrawHorizontalDimensions() +
+                  DrawSupportForces() +
+                  DrawQForce(_beam.L1, _beam.L2,_beam.Q1) +
+                  DrawQForce(_beam.L1+_beam.L2, _beam.L3,_beam.Q2) +
+                  DrawPForce(_beam.L1,_beam.P)+
+
                   "ctx.font = '20px Arial';" +
             //$"ctx.fillText('{_paramFiz.Rectangle.Height}a',5,rHigh/2);";
             "</scr" + "ipt>";
@@ -45,10 +49,67 @@ namespace ProjectCalculator.Infrastructure.DrawingScripts
                  "ctx.stroke();";
         }
 
+        private string DrawPForce(double forcePoint, double forceValue)
+        {
+            var scale = 80;
+            var forceLength = 45;
+            return
+                DrawPArrow(forcePoint*scale, forceLength) +
+                $"ctx.fillText('{forceValue}q',{forcePoint*scale}+5,{forceLength});";
+
+        }
+
+        private string DrawQForce(double firstPoint, double lastPoint, double qForce)
+        {
+            var forceHeight = 5 * qForce;
+            var scale = 80;
+            var startPoint = firstPoint * scale;
+            var endPoint = (firstPoint + lastPoint) * scale;
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(
+            $"ctx.moveTo({startPoint},{-forceHeight});" +
+            $"ctx.lineTo({endPoint},{-forceHeight});");
+
+            
+
+            var distance = startPoint;
+
+            while(distance <= endPoint)
+            {
+                stringBuilder.Append(DrawArrow(distance, forceHeight));
+                distance += 0.5*scale;
+            }
+            stringBuilder.Append($"ctx.fillText('{qForce}q',{endPoint}+5,{-forceHeight});");
+            stringBuilder.Append("ctx.stroke();");
+            return stringBuilder.ToString();
+        }
+
+        private string DrawArrow(double xCoordinate, double force)
+        {
+            return $"ctx.moveTo({xCoordinate},-{force});" +
+                 $"ctx.lineTo({xCoordinate},-5);" +
+                 $"ctx.lineTo({xCoordinate}-5,-15);" +
+                 $"ctx.moveTo({xCoordinate},-5);" +
+                 $"ctx.lineTo({xCoordinate}+5,-15);" +
+                 "ctx.stroke();";
+        }
+
+        private string DrawPArrow(double xCoordinate, double force)
+        {
+            return $"ctx.moveTo({xCoordinate},5);" +
+                 $"ctx.lineTo({xCoordinate},{force});" +
+                 $"ctx.lineTo({xCoordinate}-5,{force}-15);" +
+                 $"ctx.moveTo({xCoordinate},{force});" +
+                 $"ctx.lineTo({xCoordinate}+5,{force}-15);" +
+                 "ctx.stroke();";
+        }
+
+
+
         private string DrawSupportForces()
         {
             return "ctx.moveTo(0,25);" +
-                "ctx.lineTo(0,65);"+
+                "ctx.lineTo(0,65);" +
                 "ctx.moveTo(0,25);" +
                 "ctx.lineTo(-5,35);" +
                 "ctx.moveTo(0,25);" +
@@ -62,7 +123,7 @@ namespace ProjectCalculator.Infrastructure.DrawingScripts
                 "ctx.moveTo(-10,0);" +
                 "ctx.lineTo(-20,5);" +
                  $"ctx.fillText('Ha',-40 ,20);" +
-                  "ctx.stroke();"+
+                  "ctx.stroke();" +
 
                 "ctx.beginPath();   " +
                 "ctx.moveTo(-50,30);" +
@@ -100,7 +161,7 @@ namespace ProjectCalculator.Infrastructure.DrawingScripts
                    "ctx.lineTo(-5,10);" +
                     "ctx.moveTo(0,10);" +
                    "ctx.lineTo(-5,15);" +
-                    "ctx.moveTo(0,15);"+
+                    "ctx.moveTo(0,15);" +
                     "ctx.lineTo(-5,20);" +
                      "ctx.moveTo(0,0);" +
                    "ctx.stroke();";
@@ -109,7 +170,7 @@ namespace ProjectCalculator.Infrastructure.DrawingScripts
         private string DrawHorizontalDimensions()
         {
             return "ctx.moveTo(0,1.2*scale);" +
-                 $"ctx.lineTo({_beam.L1 + _beam.L2 + _beam.L3}*scale,1.2*scale);"+
+                 $"ctx.lineTo({_beam.L1 + _beam.L2 + _beam.L3}*scale,1.2*scale);" +
 
                  //draw first
                  "ctx.moveTo(0,1.2*scale);" +
@@ -122,22 +183,22 @@ namespace ProjectCalculator.Infrastructure.DrawingScripts
                  $"ctx.moveTo({_beam.L1}*scale,1.2*scale-5);" +
                  $"ctx.lineTo({_beam.L1}*scale,1.2*scale+5);" +
                  $"ctx.moveTo({_beam.L1}*scale,1.2*scale);" +
-                  $"ctx.fillText('{_beam.L1}a',scale*{_beam.L1/2} ,1.2*scale-5);" +
+                  $"ctx.fillText('{_beam.L1}a',scale*{_beam.L1 / 2} ,1.2*scale-5);" +
 
                  //draw third
-                 $"ctx.moveTo({_beam.L1+_beam.L2}*scale,1.2*scale);" +
-                 $"ctx.moveTo({_beam.L1+_beam.L2}*scale,1.2*scale-5);" +
-                 $"ctx.lineTo({_beam.L1+_beam.L2}*scale,1.2*scale+5);" +
                  $"ctx.moveTo({_beam.L1 + _beam.L2}*scale,1.2*scale);" +
-                  $"ctx.fillText('{_beam.L2}a',scale*{_beam.L1 + _beam.L2/2} ,1.2*scale-5);" +
+                 $"ctx.moveTo({_beam.L1 + _beam.L2}*scale,1.2*scale-5);" +
+                 $"ctx.lineTo({_beam.L1 + _beam.L2}*scale,1.2*scale+5);" +
+                 $"ctx.moveTo({_beam.L1 + _beam.L2}*scale,1.2*scale);" +
+                  $"ctx.fillText('{_beam.L2}a',scale*{_beam.L1 + _beam.L2 / 2} ,1.2*scale-5);" +
 
 
                  //draw fourth
-                 $"ctx.moveTo({_beam.L1+_beam.L2+_beam.L3}*scale,1.2*scale);" +
-                 $"ctx.moveTo({_beam.L1+_beam.L2+_beam.L3}*scale,1.2*scale-5);" +
-                 $"ctx.lineTo({_beam.L1+_beam.L2+_beam.L3}*scale,1.2*scale+5);" +
                  $"ctx.moveTo({_beam.L1 + _beam.L2 + _beam.L3}*scale,1.2*scale);" +
-                 $"ctx.fillText('{_beam.L3}a',scale*{_beam.L1 + _beam.L2 + _beam.L3/2} ,1.2*scale-5);" +
+                 $"ctx.moveTo({_beam.L1 + _beam.L2 + _beam.L3}*scale,1.2*scale-5);" +
+                 $"ctx.lineTo({_beam.L1 + _beam.L2 + _beam.L3}*scale,1.2*scale+5);" +
+                 $"ctx.moveTo({_beam.L1 + _beam.L2 + _beam.L3}*scale,1.2*scale);" +
+                 $"ctx.fillText('{_beam.L3}a',scale*{_beam.L1 + _beam.L2 + _beam.L3 / 2} ,1.2*scale-5);" +
                  "ctx.stroke();";
         }
     }
