1e28cd5aa183a0fc6fb7e717b05ea41b1b53a6fe
orzio <andrzej.orzinsk@gmail.com>
2020-08-12 21:07:10 +0200

    finish navbar

---
+++ ProjectCalculator/wwwroot/js/stylejs.js
@@ -0,0 +1,12 @@
+﻿    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+    const links = document.querySelector('.nav-link');
+    const content = document.querySelector('.content');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('nav-active');
+        document.body.classList.toggle('lock-scroll');
+        content.classList.toggle('curtain');
+        
+    });
+
