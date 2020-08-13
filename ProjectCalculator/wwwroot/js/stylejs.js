af21479fbd07d7a72bf5b2cd737785fbdf89cedd
orzio <andrzej.orzinsk@gmail.com>
2020-08-13 21:35:49 +0200

    add navbar styling

--- ProjectCalculator/wwwroot/js/stylejs.js
+++ ProjectCalculator/wwwroot/js/stylejs.js
@@ -1,12 +1,45 @@
-﻿    const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.nav-links');
-    const links = document.querySelector('.nav-link');
-    const content = document.querySelector('.content');
+﻿const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelector('.nav-link');
+const content = document.querySelector('.content');
+const middleList = document.querySelector('.middle-list');
+const rightList = document.querySelector('.right-list');
+const rightListElements = document.querySelector('.right-list').querySelectorAll("li");
 
-    hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('nav-active');
-        document.body.classList.toggle('lock-scroll');
-        content.classList.toggle('curtain');
-        
-    });
+let isMenuOpened = false;
+
+hamburger.addEventListener('click', () => {
+    if (isMenuOpened) {
+        isMenuOpened = false
+    } else {
+        isMenuOpened = true;
+    }
+    navLinks.classList.toggle('nav-active');
+    document.body.classList.toggle('lock-scroll');
+    content.classList.toggle('curtain');
+
+    console.log(isMenuOpened);
+
+    var movedElementsCount = rightList.length;
+    console.log(movedElementsCount);
+    if (isMenuOpened) {
+        for (var i = 0; i < rightListElements.length; i++) {
+            middleList.appendChild(rightListElements[i]);
+            console.log(rightListElements[i]);
+            console.log(middleList.length);
+        }
+    }
+
+    if (!isMenuOpened) {
+
+
+        for (var i = 0; i < rightListElements.length; i++) {
+            rightList.appendChild(rightListElements[i]);
+        }
+
+    }
+
+});
+
+
 
